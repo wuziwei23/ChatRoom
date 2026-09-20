61a445aa7e721e021dacffae0b9fd12ea3798620
wuziwei <3395860697@qq.com>
2026-09-20 12:15:20 +0800

    客户端代码接受广播

--- client_linux.c
+++ client_linux.c
@@ -5,6 +5,8 @@
 #include <pthread.h>
 #include <arpa/inet.h>
 
+void *receive(void *arg);
+
 int main(){
     // 创建通信的套接字
     int connect_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -27,20 +29,51 @@ int main(){
         perror("connect");
         return -1;
     }
-    
+
+    pthread_t thread;
+    pthread_create(&thread, NULL, receive, connect_fd);
+    pthread_detach(thread);
+
     while(1){
         char buf[1024];
         if(fgets(buf, sizeof(buf), stdin) == NULL){
             printf("input error...\n");
             break;
         }
-        send(connect_fd, buf, sizeof(buf), 0);
+        send(connect_fd, buf, strlen(buf), 0);
 
-        memset(buf, 0, sizeof(buf));
-        int len = recv(connect_fd, buf, sizeof(buf), 0);
+        // memset(buf, 0, sizeof(buf));
+        // int len = recv(connect_fd, buf, sizeof(buf), 0);
+        // if(len > 0){
+        //     buf[len] = '\0';
+        //     printf("%s\n", buf);
+        // }
+        // else if(len == 0){
+        //     printf("服务器已经断开\n");
+        //     break;
+        // }
+        // else{
+        //     perror("recv");
+        //     break;
+        // }
+    }
+
+
+    close(connect_fd);
+
+    return 0;
+}
+
+void *receive(void* arg){
+    int connect_fd = (int)arg;
+
+    while(1){
+        char msg[1024];
+        int len = recv(connect_fd, msg, sizeof(msg), 0);
+
         if(len > 0){
-            buf[len] = '\0';
-            printf("%s\n", buf);
+            msg[len] = '\0';
+            printf("%s", msg);
         }
         else if(len == 0){
             printf("服务器已经断开\n");
@@ -52,7 +85,5 @@ int main(){
         }
     }
 
-    close(connect_fd);
-
-    return 0;
+    return NULL;
 }
